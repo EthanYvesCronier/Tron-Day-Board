90ef856d1d7c1837a9dbe607145b602b2d5772df
Wesley Kim <71526260+wesleykim2002@users.noreply.github.com>
2020-09-25 10:04:52 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,29 @@
+/**
+ * super quack
+ */
+/**
+ * gravity
+ */
 input.buttonsAB.onEvent(ButtonEvent.Click, function () {
-	
+    makerController.player1.press(ArcadeButton.AB)
 })
+input.onLoudSound(function () {
+    makerController.player1.press(ArcadeButton.B)
+})
+/**
+ * Jetpack
+ */
+input.buttonB.onEvent(ButtonEvent.Down, function () {
+    makerController.player1.setButton(ArcadeButton.A, true)
+})
+/**
+ * flap
+ */
 input.buttonA.onEvent(ButtonEvent.Click, function () {
-	
+    makerController.player1.press(ArcadeButton.A)
+})
+input.buttonB.onEvent(ButtonEvent.Up, function () {
+    makerController.player1.setButton(ArcadeButton.A, false)
 })
 forever(function () {
     makerController.player1.setAnalog(ArcadeAnalogButton.LeftRight, input.acceleration(Dimension.X))
