ead41883fa6b1a57198173081fc56e2494fd86df
EthanYvesCronier <71526327+EthanYvesCronier@users.noreply.github.com>
2020-09-25 10:32:45 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,30 +1,79 @@
-/**
- * super quack
- */
-/**
- * gravity
- */
+// gravity
 input.buttonsAB.onEvent(ButtonEvent.Click, function () {
     makerController.player1.press(ArcadeButton.AB)
 })
 input.onLoudSound(function () {
     makerController.player1.press(ArcadeButton.B)
 })
-/**
- * Jetpack
- */
+// Jetpack
 input.buttonB.onEvent(ButtonEvent.Down, function () {
     makerController.player1.setButton(ArcadeButton.A, true)
 })
-/**
- * flap
- */
+function Sans_Intermediate () {
+    music.ringTone(117)
+    music.playTone(466, music.beat(BeatFraction.Quarter))
+    music.rest(music.beat(BeatFraction.Sixteenth))
+    music.playTone(466, music.beat(BeatFraction.Quarter))
+    music.rest(music.beat(BeatFraction.Sixteenth))
+    music.playTone(1174, music.beat(BeatFraction.Quarter))
+    music.rest(music.beat(BeatFraction.Quarter))
+    music.playTone(880, music.beat(BeatFraction.Half))
+    music.rest(music.beat(BeatFraction.Quarter))
+    music.stopAllSounds()
+    music.ringTone(131)
+    music.playTone(831, music.beat(BeatFraction.Half))
+    music.rest(music.beat(BeatFraction.Sixteenth))
+    music.playTone(784, music.beat(BeatFraction.Half))
+    music.rest(music.beat(BeatFraction.Sixteenth))
+    music.playTone(698, music.beat(BeatFraction.Half))
+    music.rest(music.beat(BeatFraction.Sixteenth))
+    music.playTone(587, music.beat(BeatFraction.Quarter))
+    music.rest(music.beat(BeatFraction.Sixteenth))
+    music.playTone(698, music.beat(BeatFraction.Quarter))
+    music.rest(music.beat(BeatFraction.Sixteenth))
+    music.playTone(784, music.beat(BeatFraction.Quarter))
+    music.stopAllSounds()
+}
 input.buttonA.onEvent(ButtonEvent.Click, function () {
     makerController.player1.press(ArcadeButton.A)
 })
 input.buttonB.onEvent(ButtonEvent.Up, function () {
     makerController.player1.setButton(ArcadeButton.A, false)
 })
-forever(function () {
-    makerController.player1.setAnalog(ArcadeAnalogButton.LeftRight, input.acceleration(Dimension.X))
+function Sans_Simple (FirstNote: number, LowNote: number) {
+    music.ringTone(LowNote)
+    music.playTone(FirstNote, music.beat(BeatFraction.Quarter))
+    music.rest(music.beat(BeatFraction.Sixteenth))
+    music.playTone(FirstNote, music.beat(BeatFraction.Quarter))
+    music.rest(music.beat(BeatFraction.Sixteenth))
+    music.playTone(1174, music.beat(BeatFraction.Quarter))
+    music.rest(music.beat(BeatFraction.Quarter))
+    music.playTone(880, music.beat(BeatFraction.Half))
+    music.rest(music.beat(BeatFraction.Quarter))
+    music.playTone(831, music.beat(BeatFraction.Half))
+    music.rest(music.beat(BeatFraction.Sixteenth))
+    music.playTone(784, music.beat(BeatFraction.Half))
+    music.rest(music.beat(BeatFraction.Sixteenth))
+    music.playTone(698, music.beat(BeatFraction.Half))
+    music.rest(music.beat(BeatFraction.Sixteenth))
+    music.playTone(587, music.beat(BeatFraction.Quarter))
+    music.rest(music.beat(BeatFraction.Sixteenth))
+    music.playTone(698, music.beat(BeatFraction.Quarter))
+    music.rest(music.beat(BeatFraction.Sixteenth))
+    music.playTone(784, music.beat(BeatFraction.Quarter))
+    music.stopAllSounds()
+}
+music.setTempo(150)
+music.setVolume(120)
+control.runInParallel(function () {
+    while (true) {
+        Sans_Simple(587, 0)
+        Sans_Simple(523, 0)
+        Sans_Simple(494, 0)
+        Sans_Simple(466, 0)
+        Sans_Simple(587, 147)
+        Sans_Simple(523, 131)
+        Sans_Simple(494, 123)
+        Sans_Intermediate()
+    }
 })
